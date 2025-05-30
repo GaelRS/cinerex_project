f21d648394963cc8c6f8257defd4d9a7eccd65fd
GaelRS <grosas04@alumnos.uaq.mx>
2025-05-30 00:45:13 -0600

    feat: Movies without documentation on swagger

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('movies')
 export class MoviesController {
@@ -12,23 +13,30 @@ export class MoviesController {
     return this.moviesService.create(createMovieDto);
   }
 
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('poster'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    return "ok"
+  }
+
   @Get()
   findAll() {
     return this.moviesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.moviesService.findOne(+id);
+  findOne(@Param('id', new ParseUUIDPipe({version: '4'})) id: string) {
+    return this.moviesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMovieDto: UpdateMovieDto) {
-    return this.moviesService.update(+id, updateMovieDto);
+  update(@Param('id', new ParseUUIDPipe({version:'4'})) id: string, @Body() updateMovieDto: UpdateMovieDto) {
+    return this.moviesService.update(id, updateMovieDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.moviesService.remove(+id);
+    return this.moviesService.remove(id);
   }
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,26 +1,61 @@
-import { Injectable } from '@nestjs/common';
-import { CreateMovieDto } from './dto/create-movie.dto';
-import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { CreateMovieDto } from "./dto/create-movie.dto";
+import { UpdateMovieDto } from "./dto/update-movie.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Movie } from "./entities/movie.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class MoviesService {
-  create(createMovieDto: CreateMovieDto) {
-    return 'This action adds a new movie';
+  constructor(
+    @InjectRepository(Movie)
+    private readonly movieRepository: Repository<Movie>
+  ) {}
+
+  async create({ functions = [], ...rest }: CreateMovieDto) {
+    const movieToSave: any = {
+      ...rest,
+      functions: functions.map((id: string) => ({ id })),
+    };
+
+    this.movieRepository.save(movieToSave);
+    return movieToSave;
   }
 
   findAll() {
-    return `This action returns all movies`;
+    return this.movieRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} movie`;
+  async findOne(id: string) {
+    const movie = await this.movieRepository.findOne({
+      where: { movieId: id },
+    });
+    if (!movie) throw new NotFoundException("Movie not found");
+    return movie;
   }
 
-  update(id: number, updateMovieDto: UpdateMovieDto) {
-    return `This action updates a #${id} movie`;
+  async update(id: string, updateMovieDto: UpdateMovieDto) {
+    const { functions, ...rest } = updateMovieDto;
+    const movieToUpdate: any = {
+      movieId: id,
+      ...rest,
+      functions: functions?.map((id: string) => ({ id })),
+    };
+
+    const movieUpdated = await this.movieRepository.preload(movieToUpdate);
+    if (!movieUpdated) {
+      throw new NotFoundException("Movie not found");
+    }
+
+    return await this.movieRepository.save(movieUpdated);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} movie`;
+  remove(id: string) {
+    const movie = this.movieRepository.findOne({ where: { movieId: id } });
+    if (!movie) {
+      throw new NotFoundException("Movie not found");
+    }
+    this.movieRepository.delete(id);
+    return { message: `Movie ${id} deleted successfully` };
   }
 }
